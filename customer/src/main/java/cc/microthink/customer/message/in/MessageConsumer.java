ec38cacef92bf66efdfe8280c8a163ef8c021b8c
SharpQin <sharp.xx@yahoo.com>
2022-06-18 15:08:44 +0800

    Add message consumer for customer

--- customer/src/main/java/cc/microthink/customer/message/in/MessageConsumer.java
+++ customer/src/main/java/cc/microthink/customer/message/in/MessageConsumer.java
@@ -6,6 +6,7 @@ import cc.microthink.customer.service.CustomerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Consumer;
@@ -22,24 +23,57 @@ public class MessageConsumer {
     }
 
     /**
+     * DB processing
+     * @param messageUser
+     */
+    private void processMKUser(MessageMKUser messageUser) {
+        if (messageUser.isCreate()) {
+            Customer customer = new Customer(messageUser.getUser());
+            //save or update base on orm framework
+            customerService.save(customer);
+        }
+        else if (messageUser.isUpdate()) {
+            Customer customer = new Customer(messageUser.getUser());
+            customerService.update(customer);
+        }
+        else if (messageUser.isDelete()) {
+            customerService.delete(messageUser.getUser().getId());
+        }
+    }
+
+    /**
      * Consume event when create a new MKUser
      * @return
      */
     @Bean
-    public Consumer<MessageMKUser> onMKUserActionConsumer() {
-        return messageUser -> {
+    public Consumer<Message<MessageMKUser>> onMKUserActionConsumer() {
+        return message -> {
+
+            /* //autoCommitOffset=false and commit acknowledgment manually
+            Acknowledgment acknowledgment = message.getHeaders().get(KafkaHeaders.ACKNOWLEDGMENT, Acknowledgment.class);
+            log.debug("onMKUserActionConsumer: acknowledgment:{}", acknowledgment);
+            if (acknowledgment != null) {
+                acknowledgment.acknowledge();
+            }
+            */
+
+            MessageMKUser messageUser = message.getPayload();
             log.info("onMKUserActionConsumer: messageUser:{}", messageUser);
-            if (messageUser.isCreate()) {
-                Customer customer = new Customer(messageUser.getUser());
-                customerService.save(customer);
-            }
-            else if (messageUser.isUpdate()) {
-                Customer customer = new Customer(messageUser.getUser());
-                customerService.update(customer);
-            }
-            else if (messageUser.isDelete()) {
-                customerService.delete(messageUser.getUser().getId());
-            }
+            processMKUser(messageUser);
+        };
+    }
+
+    /**
+     * Consume dlq message
+     * @return
+     */
+    @Bean
+    public Consumer<MessageMKUser> onMKUserFailConsumer() {
+        return messageMKUser -> {
+            log.debug("###### onMKUserFailConsumer ######");
+            log.debug("onMKUserFailConsumer: action:{}, id:{}, name:{}", messageMKUser.getAction(), messageMKUser.getUser().getId(),messageMKUser.getUser().getLogin());
+            log.debug("onMKUserFailConsume: messageMKUser{}", messageMKUser);
+            processMKUser(messageMKUser);
         };
     }
 }
